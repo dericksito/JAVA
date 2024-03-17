a5cf0c1ab3571767b3e321cf8454c529ef1e9492
dericksito <derickcedeno2@icloud.com>
2024-03-17 01:23:58 -0500

    Reto-14/ video resolviendo ejercicio completo

---
+++ Demo/src/com/krakedev/test/TestProducto.java
@@ -0,0 +1,21 @@
+package com.krakedev.test;
+
+import com.krakedev.demo.Producto;
+
+public class TestProducto {
+
+	public static void main(String[] args) {
+		Producto produ=new Producto(12345,"Papitas");
+		
+		produ.setDescripcion("Rufless");
+		produ.setPeso(56.8);
+		
+		System.out.println("-----PRODUCTOS AGREGADOS------");
+		System.out.println("Codigo: "+produ.getCodigo());
+		System.out.println("Nombre: "+produ.getNombre());
+		System.out.println("Descripcion: "+produ.getDescripcion());
+		System.out.println("Peso: "+produ.getPeso());
+
+	}
+
+}
