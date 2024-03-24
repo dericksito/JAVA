509c8ac8be48ce51623e55ab77a7465e2095d552
dericksito <derickcedeno2@icloud.com>
2024-03-24 01:55:08 -0500

    ACTUALIZACIOON DE CODIGO/HASTA EJECUTAR MAVEN

---
+++ ejemplomaven/src/main/java/com/krakedev/ejemplomaven/Admin.java
@@ -0,0 +1,20 @@
+package com.krakedev.ejemplomaven;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+private static Logger logger=LogManager.getLogger(Admin.class);//asi creamos una instancia  y tiene q recifir como parametro la clase a la q se le aplica
+//entonces este logger esta atado a mi clase admin en este caso
+
+
+public void agregar() {
+	
+	logger.info("mensaje de info ");
+	logger.warn("mensaje de warn ");
+	logger.trace("mensaje de trace ");
+	logger.error("mensaje de error ");
+	logger.debug("mensaje de debug ");
+}
+}
+ 
